ca1af0e7dbdb5a61ce410256d7df87c2ef3962ba
Sebas de la Torre <lucasebasdlt@gmail.com>
2026-02-16 21:23:38 -0300

    chore:stack inicial react & typescript

---
+++ JC/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const baseUrl = import.meta.env.VITE_BASE_URL
+
+  return (
+    <>
+      
+
+    </>
+  )
+}
+
+export default App
